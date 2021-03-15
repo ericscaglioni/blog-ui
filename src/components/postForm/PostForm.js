268e80f4040ccdcd39b2593bdb0e862d3ff5277c
Eric Ferreira <eferreira@fullstacklabs.co>
2021-03-15 12:04:28 -0300

    feat: add form style and validations

--- src/components/postForm/PostForm.js
+++ src/components/postForm/PostForm.js
@@ -1,7 +1,19 @@
+import { makeStyles, TextField, Button } from '@material-ui/core'
 import axios from 'axios'
 import React, { useState } from 'react'
 
+const REQUIRED_FIELD = 'Campo obrigatorio'
+
+const useStyles = makeStyles(theme => ({
+    textField: {
+        marginBottom: theme.spacing(1)
+    }
+}))
+
 export const PostForm = ({ setPosts }) => {
+    const classes = useStyles()
+
+    const [submitted, setSubmitted] = useState(false)
     const [post, setPost] = useState({
         title: '',
         text: ''
@@ -10,6 +22,8 @@ export const PostForm = ({ setPosts }) => {
     const submit = async (e) => {
         try {
             e.preventDefault()
+            setSubmitted(true)
+            if (!isValid()) return
             const { data } = await axios.post(
                 'http://localhost:8001/api/posts',
                 post
@@ -18,13 +32,23 @@ export const PostForm = ({ setPosts }) => {
                 ...previousList,
                 ...[data]
             ])
-            setPost({
-                title: '',
-                text: ''
-            })
+            clearForm()
         } catch (error) {
             console.error('Deu erro aqui', error)
         }
+    }
+
+    const clearForm = () => {
+        setSubmitted(false)
+        setPost({
+            title: '',
+            text: ''
+        })
+    }
+
+    const isValid = () => {
+        if (!post.title || !post.text) return false
+        return true
     }
 
     const handleTitleOnChange = e => {
@@ -44,12 +68,36 @@ export const PostForm = ({ setPosts }) => {
     }
 
     return (
-        <form onSubmit={submit}>
-            <label>Title</label>
-            <input type='text' onChange={handleTitleOnChange} value={post.title}/>
-            <label>Text</label>
-            <input type='text' onChange={handleTextOnChange} value={post.text}/>
-            <button type='submit' color='primary'>Submit</button>
+        <form onSubmit={submit} noValidate autoComplete='off'>
+            <div>
+                <TextField
+                    fullWidth
+                    required
+                    id='title'
+                    label='Title'
+                    onChange={handleTitleOnChange}
+                    value={post.title}
+                    className={classes.textField}
+                    error={submitted && !post.title}
+                    helperText={submitted && !post.title && REQUIRED_FIELD}
+                />
+            </div>
+            <div>
+                <TextField
+                    fullWidth
+                    required
+                    id='text'
+                    label='Text'
+                    onChange={handleTextOnChange}
+                    value={post.text}
+                    className={classes.textField}
+                    error={submitted && !post.text}
+                    helperText={submitted && !post.text && REQUIRED_FIELD}
+                />
+            </div>
+            <Button fullWidth type='submit' variant='contained' color='primary' size="large">
+                Submit
+            </Button>
         </form>
     )
 }
